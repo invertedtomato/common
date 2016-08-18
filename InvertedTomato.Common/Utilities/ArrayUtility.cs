0d7c6cba80a23758c21fe0ddc0da068ca6985684
Ben Thompson <ben@invertedtomato.com>
2016-08-18 16:05:25 +1000

    Tidy.

--- InvertedTomato.Common/Utilities/ArrayUtility.cs
+++ InvertedTomato.Common/Utilities/ArrayUtility.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InvertedTomato {
+    [Obsolete("Poorly designed. Will be removed in future release.")]
     public static class ArrayUtility {
         /// <summary>
         /// Returns True if contents are identical, False otherwise.
