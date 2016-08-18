e711ae8290c6e660d6f1d5edb7ec8d4b45877ee7
Ben Thompson <ben@invertedtomato.com>
2016-08-18 15:24:19 +1000

    Added flawed notice.

--- InvertedTomato.Common/Utilities/MacAddressUtility.cs
+++ InvertedTomato.Common/Utilities/MacAddressUtility.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 
 namespace InvertedTomato {
+    [Obsolete("This is flawed, to be removed in a future release.")]
     public static class MacAddressUtility {
         /// <summary>
         /// Converts the mac address string to a byte array.
@@ -9,6 +10,7 @@ namespace InvertedTomato {
         /// <param name="macAddress">The mac address.</param>
         /// <param name="fromBase">The from base of the input string.</param>
         /// <returns></returns>
+        [Obsolete("This is flawed, to be removed in a future release.")]
         public static byte[] ToMacAddress(this string macAddress, int fromBase) {
             var macAddressArray = macAddress.Split(':').Select(b => Convert.ToByte(b, fromBase)).ToArray();
             return macAddressArray;
